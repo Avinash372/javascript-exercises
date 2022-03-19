36f161ad0c81505b375127dad1164bf6ceea497e
Avinash372 <avinash.sahu3123@gmail.com>
2022-03-20 00:26:45 +0530

    completed 4 exercise so far

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...rm) {
+    let input = array;
+    let rmArray = [...rm];
+    
+    for(let i = 0; i<input.length; i++){
+      let result = input.filter(function(n) { return rmArray.indexOf(n) == -1;});
+    return result;
+    }
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
