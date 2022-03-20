85b7df395c4909e54d2eeae57d3e6be5bb99ecf4
Avinash372 <avinash.sahu3123@gmail.com>
2022-03-20 23:26:09 +0530

    completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1,num2, ...num) {
+	let added = num1 + num2 + num;
+  return +added;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return sub = num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  let array = args[0];
+  let result = 0;
+  for(let i = 0; i<array.length; i++){
+    result+=array[i];
+  }
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(...args) {
+  let array = args[0];
+  let result = 1;
+  for(let i = 0; i<array.length; i++){
+    result*=array[i];
+  }
+  return result;};
 
+const power = function(num, pow) {
+  let result = num;
+  for(let i = 0; i<pow-1; i++){
+    result*= num;
+  }
+  return result;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1
+  let i = 1
+  while(i<=num){
+    result*=i;
+    i++;
+  }
+  return result;
 };
 
 // Do not edit below this line
