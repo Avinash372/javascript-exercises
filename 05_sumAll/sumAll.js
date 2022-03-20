2606da6c1b7c1940d63ac121d5cb2997564c385d
Avinash372 <avinash.sahu3123@gmail.com>
2022-03-20 18:10:57 +0530

    completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+  let sum = 0;
+  if (min === +min && max === +max && min >= 0 && max >= 0) {
+    if (min <= max) {
+      for (let i = min; i <= max; i++) {
+        sum += i;
+      }
+    }
+    if (min >= max) {
+      for (i = min; i >= max; i--) {
+        sum += i;
+      }
+    }
+  } else {
+    sum = "ERROR";
+  }
+  return sum;
 };
 
 // Do not edit below this line
