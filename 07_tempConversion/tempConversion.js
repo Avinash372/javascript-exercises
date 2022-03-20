042ddbdddfe1320560dbe3a361233091e44cc2aa
Avinash372 <avinash.sahu3123@gmail.com>
2022-03-20 19:48:26 +0530

    completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celsius = ((fahrenheit-32)*(5/9)).toFixed(1);
+  return +celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let fahrenheit = (celsius*9/5 +32).toFixed(1);
+  return +fahrenheit;
 };
 
 // Do not edit below this line
