53a0ce75ddfff5a55d2404b9dc408546842a4450
Avinash372 <avinash.sahu3123@gmail.com>
2022-03-21 16:50:12 +0530

    completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (input) {
+    let mArray = input.replace(/[^a-zA-Z]/g, "").toLowerCase();
+    let result = '';
+    for(let i = mArray.length-1; i>=0; i--){
+      result+=mArray[i]
+    }
+    if(result===mArray){
+      return true
+    } else {
+      return false
+    }
+    return result;
+  };
+console.log(palindromes("!A car, a man, a maraca"))
 // Do not edit below this line
 module.exports = palindromes;
