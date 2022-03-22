2740b0104030778ca7ee5501471d49c37afb6299
Avinash372 <avinash.sahu3123@gmail.com>
2022-03-22 08:01:56 +0530

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number>0 && number == +number){
+        let result= [0,1];
+        for(let i = 0; i<=number-2; i++){
+            let series = result[i] + result[i+1]
+            result.push(series);
+        }
+        return result[number];
+    } else {
+        return "OOPS"
+    }
 };
-
+console.log(fibonacci("10"))
 // Do not edit below this line
 module.exports = fibonacci;
